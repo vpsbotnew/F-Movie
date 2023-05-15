7b113168e923ca3ef85ce7c495d8edae0b56636d
vpsbotnew <121183724+vpsbotnew@users.noreply.github.com>
2023-05-15 19:48:35 +0630

    Update config.py

--- mks/config/config.py
+++ mks/config/config.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
     
 class Config:
 
-    PORT = environ.get("PORT", "8006")
+    PORT = environ.get("PORT", "8008")
     SESSION = environ.get('SESSION', 'Media_search')
     
     API_ID = int(environ.get('API_ID', "12158462"))
